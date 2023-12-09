d90e62ba884e33f9de5ec0c540206904384be8ca
arunlajayan <arunlajayan@gmail.com>
2023-12-09 14:32:16 +0530

    apoolo server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require('path');
 // const { buildSchema } = require("graphql");
-const { graphqlHTTP } = require("express-graphql");
+// const { graphqlHTTP } = require("express-graphql");
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-
+const { ApolloServer } = require('apollo-server-express');
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
 
 const resolversArray = loadFilesSync(path.join(__dirname,'**/*.resolver.js'))
@@ -14,30 +14,46 @@ type Query{
     Orders: [Order]
 }
 `
+async function startApolloServer() {
+  const app = express();
 
-const schema = makeExecutableSchema({
+  const schema = makeExecutableSchema({
     typeDefs: typesArray,
     resolvers:resolversArray
-})
-const app = express();
+  })
+  
+  const server = new ApolloServer({
+    schema: schema,
+    
+  });
+
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' })
+  
+  
+  
+app.listen(3000, () => {
+  console.log("graphql is running");
+});
+}
+
+startApolloServer();
+
 
 // const schema = buildSchema(`
 
 
 // `);
-const root = {
-  Products: require('./Products/products.model'),
-  Orders: require('./Orders/orders.model')
-};
+// const root = {
+//   Products: require('./Products/products.model'),
+//   Orders: require('./Orders/orders.model')
+// };
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: schema,
+//     graphiql: true,
+//   })
+// );
 
-app.listen(3000, () => {
-  console.log("port is running");
-});
